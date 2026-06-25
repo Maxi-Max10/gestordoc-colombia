a5381db02122327c6446e367221dac124c333147
Rodrigo Lopez <exrodrigo>
2026-06-25 13:29:45 +0000

    ajuste: modificar posición de la firma del empleado en el PDF

--- webapp/controller/functions/contratoAprendizajeProductivo.js
+++ webapp/controller/functions/contratoAprendizajeProductivo.js
@@ -1411,7 +1411,7 @@ sap.ui.define([
                     if (pageIndex === contentBlocks.length - 1) {
                         newPage.drawText("[[FIRMA_EMPLEADO]]", {
                             x: width * 0.63,
-                            y: 210,
+                            y: 110,
                             size: 6,
                             color: PDFLibRef.rgb(1, 1, 1)
                         });
